c045990beafb172f32417e7bd8ca4349637417bf
sdhnsai <sadhanasai.g@gmail.com>
2021-04-16 08:06:42 -0400

    origin

---
+++ attribute.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAWS4H3H53LSOGVUM3"
+  secret_key = "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"
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+output "eip" {
+  value = aws_eip.lb.public_ip
+}
+
+resource "aws_s3_bucket" "mys3" {
+  bucket = "anikas3"
+}
+
+output "mys3bucket" {
+  value = aws_s3_bucket.mys3.bucket_domain_name
+}
+
