322b4b52d3f9ecfb37238e40b5e5cfc2edd2145f
betawiins <54180094+sdhnsai@users.noreply.github.com>
2021-04-16 17:19:26 +0530

    Update attribute.tf

--- attribute.tf
+++ attribute.tf
@@ -1,9 +1,9 @@
-provider "aws" {
+/*provider "aws" {
   region     = "us-west-2"
   access_key = "AKIAWS4H3H53LSOGVUM3"
   secret_key = "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"
 }
-
+*/
 resource "aws_eip" "lb" {
   vpc      = true
 }
